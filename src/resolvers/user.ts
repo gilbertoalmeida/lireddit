9a6c497f1d34bab180d731a6058fa45918a73af8
Gilberto Almeida <almeidanetogilberto@gmail.com>
2020-09-18 16:50:53 +0200

    login mutation and some validation in both login and register

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,5 +1,13 @@
-import { Resolver, Mutation, InputType, Field, Arg, Ctx } from "type-graphql";
-import { MyContext } from "src/types";
+import {
+  Resolver,
+  Mutation,
+  InputType,
+  Field,
+  Arg,
+  Ctx,
+  ObjectType
+} from "type-graphql";
+import { MyContext } from "../types";
 import { User } from "../entities/User";
 import argon2 from "argon2";
 
@@ -12,19 +20,103 @@ class UsernamePasswordInput {
   password: string;
 }
 
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+// used for return from the mutations
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[]; // the question mark is for having undefined
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
 @Resolver()
 export class UserResolver {
-  @Mutation(() => User)
+  @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
     @Ctx() { em }: MyContext
-  ) {
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "length must be greater than 2"
+          }
+        ]
+      };
+    }
+
+    if (options.password.length <= 3) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "length must be greater than 3"
+          }
+        ]
+      };
+    }
+
     const hashedPassword = await argon2.hash(options.password);
     const user = em.create(User, {
       username: options.username,
       password: hashedPassword
     });
-    await em.persistAndFlush(user);
-    return user;
+    try {
+      await em.persistAndFlush(user);
+    } catch (err) {
+      // these are the errors that the database will throw back if persist and flush fails for some reason.
+      if (err.code === "23505") {
+        //|| err.detail.includes("already exists")
+        //username already exists in the database
+        return {
+          errors: [
+            {
+              field: "username",
+              message: "username already taken"
+            }
+          ]
+        };
+      }
+    }
+    return {
+      user
+    };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = (await em.findOne(User, {
+      username: options.username
+    })) as User;
+    if (!user) {
+      return {
+        errors: [{ field: "username", message: "This username doesn't exist" }]
+      };
+    }
+    const valid = await argon2.verify(user.password, options.password);
+
+    if (!valid) {
+      return {
+        errors: [{ field: "password", message: "incorrect" }]
+      };
+    }
+
+    return {
+      user
+    };
   }
 }
