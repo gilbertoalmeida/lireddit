1648c9b45d1a1c845b7cb3e177ece142f2024cf1
Gilberto Almeida <almeidanetogilberto@gmail.com>
2020-09-19 03:35:08 +0200

    Error handling register frontend. Registering, redirecting to main page. Changed the register mutation to not use persistandflush anymore (having problems), bit the query builder in its place

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -11,6 +11,7 @@ import {
 import { MyContext } from "../types";
 import { User } from "../entities/User";
 import argon2 from "argon2";
+import { EntityManager } from "@mikro-orm/postgresql";
 
 // instead of heaving a lot of Args in the mutation, I can have only one and pass this class
 @InputType()
@@ -80,12 +81,19 @@ export class UserResolver {
     }
 
     const hashedPassword = await argon2.hash(options.password);
-    const user = em.create(User, {
-      username: options.username,
-      password: hashedPassword
-    });
+    let user;
     try {
-      await em.persistAndFlush(user);
+      const result = await (em as EntityManager)
+        .createQueryBuilder(User)
+        .getKnexQuery()
+        .insert({
+          username: options.username,
+          password: hashedPassword,
+          created_at: new Date(),
+          updated_at: new Date()
+        })
+        .returning("*");
+      user = result[0];
     } catch (err) {
       // these are the errors that the database will throw back if persist and flush fails for some reason.
       if (err.code === "23505") {
--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,27 +1,28 @@
 import React from "react";
 import { Formik, Form } from "formik";
-import {
-  FormControl,
-  FormLabel,
-  Input,
-  FormErrorMessage,
-  Box,
-  Button
-} from "@chakra-ui/core";
+import { Box, Button } from "@chakra-ui/core";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
   const [, register] = useRegisterMutation(); //the name register inside the [] is the name we are choosing to call this mutation inside the onSubmit
+  const router = useRouter();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={async values => {
+        onSubmit={async (values, { setErrors }) => {
           const response = await register(values);
+          if (response.data === undefined || response.data.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data.register.user) {
+            router.push("/");
+          }
         }}
       >
         {({ isSubmitting }) => (
